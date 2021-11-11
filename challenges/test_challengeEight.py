9179f87dfa182cffab0b8a97ce2ed27cd6d777c5
Felipe <felipe.chora@stgconsulting.com>
2021-11-10 23:34:07 -0700

    working on challenge 8

--- challenges/test_challengeEight.py
+++ challenges/test_challengeEight.py
@@ -1,11 +1,17 @@
 import requests
+import json
 
 class Testchallenges():
     def test_challengeOne(self):
         url = "https://www.copart.com/public/lots/search"
         myobj = {'query': 'toyota'}
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-        x = requests.post(url, data=myobj)
-        print(x.text)
+        x = requests.post(url, data=myobj,headers=headers)
+        #print(x.text)
+        data = json.loads(x.text)
+        total = "returnCode" in data
+        print(data['data']['results']['totalElements'])
+
         assert x.status_code == 200
 
