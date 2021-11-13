2114c5745fd4816dfb570fdf6cd942da3998d130
Felipe <felipe.chora@stgconsulting.com>
2021-11-13 16:01:20 -0700

    Add requirements file

--- conftest.py
+++ conftest.py
@@ -1,12 +1,15 @@
 import pytest
 from selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 @pytest.fixture(scope="class")
 def setup(request):
-    # driver = webdriver.Chrome(executable_path="C:\\Users\\Felipe\\PycharmProjects\\pythonSeleniumFramework\\chromedriver.exe")
-
-    driver = webdriver.Chrome(
-        executable_path="/Users/felipechora/PycharmProjects/PythonSeleniumFramework/chromedriver")
+    s = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=s)
+    driver.maximize_window()
     request.cls.driver = driver
     yield
     driver.close()
