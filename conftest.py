3414cb583e9445276a7789cd6f49a73fd0fd4c7c
Felipe <felipe.chora@stgconsulting.com>
2021-11-13 21:35:19 -0700

    updated chromedriver

--- conftest.py
+++ conftest.py
@@ -4,11 +4,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
 
 @pytest.fixture(scope="class")
 def setup(request):
-    s = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=s)
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.maximize_window()
     request.cls.driver = driver
     yield
