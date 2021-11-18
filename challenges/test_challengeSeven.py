eb02067e6db0dc2c4ae195b2cf7292f929ca7018
Bryan Davie <bsdavie@coopbenefitsgroup.com>
2021-11-18 09:09:24 -0700

    I looked through challenges 1-7, they look pretty good. I did have some questions on challenge 7. Please take a look at those and if possible get a fix in before we meet.

--- challenges/test_challengeSeven.py
+++ challenges/test_challengeSeven.py
@@ -8,3 +8,12 @@ class Testchallenges(BaseClass):
         copa.getMakes()
         #test1.cleckSearch()
         print("Hello world")
+
+        # This one looks a bit off to me.
+        # Line 9 has something that seems to be leftover from troubleshooting possibly.
+        # If this is no longer needed lets remove it.
+        # I see that it prints the Make and URL associated this looks good.
+        # It then does an assert on only one entry, from what I can see, in the Dictionary and prints that one.
+        # I believe one of the challenge requirements is to verify all the elements go to the correct URL.
+        # The extra Hello World could be taken out but is really fine.
+        # If you have time to look at this and get a fix in before we meet that would be great.
